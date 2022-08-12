a3f3379b2e10ca1f9263691aea407d8e30f9bc3c
zhang-zh <1563833973@qq.com>
2022-08-12 23:17:26 +0800

    functional version 2

--- include/hex8nodeEle.hpp
+++ include/hex8nodeEle.hpp
@@ -26,31 +26,31 @@ struct hex8node : public shapeInterface
 
 private:
 #ifndef NOT_KEEP_BMat
-    void calBMatrix(MatrixXr &TV, realn E, realn nu);
+    void calBMatrix(MatrixXr &TV);
 #else
-    void calBMatrix(MatrixXr &TV, realn E, realn nu, funcMat &ele_BMat, funcMat &Jacobi) const;
+    void calBMatrix(MatrixXr &TV, funcMat &ele_BMat, funcMat &Jacobi) const;
 #endif
     void calDMatrix(realn E, realn nu, MatrixXr &ele_DMat) const;
 };
 
 void hex8node::calStiffnessMat(MatrixXr &TV, realn E, realn nu)
 {
-// #ifndef NOT_KEEP_BMat
-//     ele_BMat.resize(6,12);
-// #else
-//     MatrixXr ele_BMat(6,12);
-// #endif
     MatrixXr ele_DMat(6,6);
-    #define x(i) TV(node_num[i-1],0)
-    #define y(i) TV(node_num[i-1],1)
-    #define z(i) TV(node_num[i-1],2)
+#ifdef NOT_KEEP_BMat
+    hex8node::funcMat ele_BMat;
+    hex8node::funcMat Jacobi;
+    calBMatrix(TV, ele_BMat, Jacobi);
+#else
+    calBMatrix(TV);
+#endif
+    calDMatrix(E, nu, ele_DMat);
+    
 }
 
 #ifndef NOT_KEEP_BMat
-void hex8node::calBMatrix(MatrixXr &TV, realn E, realn nu)
+void hex8node::calBMatrix(MatrixXr &TV)
 #else
-void hex8node::calBMatrix(MatrixXr &TV, realn E, realn nu, 
-        hex8node::funcMat &ele_BMat, hex8node::funcMat &Jacobi) const
+void hex8node::calBMatrix(MatrixXr &TV, hex8node::funcMat &ele_BMat, hex8node::funcMat &Jacobi) const
 #endif
 {
     #define x(i) TV(node_num[i-1],0)
@@ -59,44 +59,69 @@ void hex8node::calBMatrix(MatrixXr &TV, realn E, realn nu,
     std::vector<int> xi    = {0,1,1,-1,-1,1,1,-1,-1};
     std::vector<int> eta   = {0,-1,1,1,-1,-1,1,1,-1};
     std::vector<int> gamma = {0,-1,-1,-1,-1,1,1,1,1};
-    // std::vector<std::function<realn(realn,realn,realn)>> dNds(9), dNdt(9), dNdn(9);
-    // for(int i=1; i<9; i++){
-    //     dNds[i] = [&xi, &eta, &gamma, &i](realn s, realn t, realn n){
-    //         return xi[i]*(1+eta[i]*t)*(1+gamma[i]*n)/8;
-    //     };
-    //     dNdt[i] = [&xi, &eta, &gamma, &i](realn s, realn t, realn n){
-    //         return eta[i]*(1+xi[i]*s)*(1+gamma[i]*n)/8;
-    //     };
-    //     dNdn[i] = [&xi, &eta, &gamma, &i](realn s, realn t, realn n){
-    //         return gamma[i]*(1+xi[i]*s)*(1+eta[i]*t)/8;
-    //     };
-    // }
-    #define dNds(NUM,FIR,SEC,THR) xi[NUM]*(1+eta[NUM]*SEC)*(1+gamma[NUM]*THR)/8
-    #define dNdt(NUM,FIR,SEC,THR) eta[NUM]*(1+xi[NUM]*FIR)*(1+gamma[NUM]*THR)/8
-    #define dNdn(NUM,FIR,SEC,THR) gamma[NUM]*(1+xi[NUM]*FIR)*(1+eta[NUM]*SEC)/8
+    #define dNds(NUM,FIR,SEC,THR) (xi[NUM]*(1+eta[NUM]*SEC)*(1+gamma[NUM]*THR)/8)
+    #define dNdt(NUM,FIR,SEC,THR) (eta[NUM]*(1+xi[NUM]*FIR)*(1+gamma[NUM]*THR)/8)
+    #define dNdn(NUM,FIR,SEC,THR) (gamma[NUM]*(1+xi[NUM]*FIR)*(1+eta[NUM]*SEC)/8)
     std::function<realn(realn,realn,realn)> dxds, dxdt, dxdn, dyds, dydt, dydn, dzds, dzdt, dzdn;
-    dxds = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNds(i,s,t,n); return res;};
-    dyds = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNds(i,s,t,n); return res;};
-    dzds = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNds(i,s,t,n); return res;};
-    dxdt = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNdt(i,s,t,n); return res;};
-    dydt = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNdt(i,s,t,n); return res;};
-    dzdt = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNdt(i,s,t,n); return res;};
-    dxdn = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNdn(i,s,t,n); return res;};
-    dydn = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNdn(i,s,t,n); return res;};
-    dzdn = [&](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNdn(i,s,t,n); return res;};
+    #define Cap_List_1  xi,eta,gamma,&TV,this
+    dxds = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNds(i,s,t,n); return res;};
+    dyds = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNds(i,s,t,n); return res;};
+    dzds = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNds(i,s,t,n); return res;};
+    dxdt = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNdt(i,s,t,n); return res;};
+    dydt = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNdt(i,s,t,n); return res;};
+    dzdt = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNdt(i,s,t,n); return res;};
+    dxdn = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += x(i)*dNdn(i,s,t,n); return res;};
+    dydn = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += y(i)*dNdn(i,s,t,n); return res;};
+    dzdn = [Cap_List_1](realn s, realn t, realn n){realn res=0; for(int i=1; i<9; i++) res += z(i)*dNdn(i,s,t,n); return res;};
     /* Calculate Jacobi matrix */
-    Jacobi = [&](realn s, realn t, realn n){
+    #define Cap_list_2  xi,eta,gamma,dxds,dxdt,dxdn,dyds,dydt,dydn,dzds,dzdt,dzdn,&TV,this
+    Jacobi = [Cap_list_2](realn s, realn t, realn n){
         MatrixXr result(3,3);
         result << dxds(s,t,n), dyds(s,t,n), dzds(s,t,n),
                   dxdt(s,t,n), dydt(s,t,n), dzdt(s,t,n),
                   dxdn(s,t,n), dydn(s,t,n), dzdn(s,t,n);
         return result;
     };
-    auto Jdet = [&](realn s, realn t, realn n){
-        return Jacobi(s,t,n).determinant();  
-    };
     /* Calculate B matrix */
-
+    #define coef_a(FIR,SEC,THR) (dydt(FIR,SEC,THR)*dzdn(FIR,SEC,THR)-dzdt(FIR,SEC,THR)*dydn(FIR,SEC,THR))
+    #define coef_b(FIR,SEC,THR) (dyds(FIR,SEC,THR)*dzdn(FIR,SEC,THR)-dzds(FIR,SEC,THR)*dydn(FIR,SEC,THR))
+    #define coef_c(FIR,SEC,THR) (dyds(FIR,SEC,THR)*dzdt(FIR,SEC,THR)-dzds(FIR,SEC,THR)*dydt(FIR,SEC,THR))
+    #define coef_d(FIR,SEC,THR) (dxdt(FIR,SEC,THR)*dzdn(FIR,SEC,THR)-dzdt(FIR,SEC,THR)*dxdn(FIR,SEC,THR))
+    #define coef_e(FIR,SEC,THR) (dxds(FIR,SEC,THR)*dzdn(FIR,SEC,THR)-dzds(FIR,SEC,THR)*dxdn(FIR,SEC,THR))
+    #define coef_f(FIR,SEC,THR) (dxds(FIR,SEC,THR)*dzdt(FIR,SEC,THR)-dzds(FIR,SEC,THR)*dxdt(FIR,SEC,THR))
+    #define coef_g(FIR,SEC,THR) (dxdt(FIR,SEC,THR)*dydn(FIR,SEC,THR)-dydt(FIR,SEC,THR)*dxdn(FIR,SEC,THR))
+    #define coef_h(FIR,SEC,THR) (dxds(FIR,SEC,THR)*dydn(FIR,SEC,THR)-dyds(FIR,SEC,THR)*dxdn(FIR,SEC,THR))
+    #define coef_i(FIR,SEC,THR) (dxds(FIR,SEC,THR)*dydt(FIR,SEC,THR)-dyds(FIR,SEC,THR)*dxdt(FIR,SEC,THR))
+#ifndef NOT_KEEP_BMat
+    #define Cap_list_3  xi,eta,gamma,dxds,dxdt,dxdn,dyds,dydt,dydn,dzds,dzdt,dzdn,&TV,this
+#else
+    #define Cap_list_3  xi,eta,gamma,dxds,dxdt,dxdn,dyds,dydt,dydn,dzds,dzdt,dzdn,&TV,this,&Jacobi
+#endif
+    ele_BMat = [Cap_list_3](realn s, realn t, realn n){
+        MatrixXr res(6,24);
+        for(int i=0; i<8; i++){
+            res(0, 3*i+0) =  coef_a(s,t,n)*dNds(i+1,s,t,n) - coef_b(s,t,n)*dNdt(i+1,s,t,n) + coef_c(s,t,n)*dNdn(i+1,s,t,n);
+            res(0, 3*i+1) =  0;
+            res(0, 3*i+2) =  0;
+            res(1, 3*i+0) =  0;
+            res(1, 3*i+1) = -coef_d(s,t,n)*dNds(i+1,s,t,n) + coef_e(s,t,n)*dNdt(i+1,s,t,n) - coef_f(s,t,n)*dNdn(i+1,s,t,n);
+            res(1, 3*i+2) =  0;
+            res(2, 3*i+0) =  0;
+            res(2, 3*i+1) =  0;
+            res(2, 3*i+2) =  coef_g(s,t,n)*dNds(i+1,s,t,n) - coef_h(s,t,n)*dNdt(i+1,s,t,n) + coef_i(s,t,n)*dNdn(i+1,s,t,n);
+            res(3, 3*i+0) = -coef_d(s,t,n)*dNds(i+1,s,t,n) + coef_e(s,t,n)*dNdt(i+1,s,t,n) - coef_f(s,t,n)*dNdn(i+1,s,t,n);
+            res(3, 3*i+1) =  coef_a(s,t,n)*dNds(i+1,s,t,n) - coef_b(s,t,n)*dNdt(i+1,s,t,n) + coef_c(s,t,n)*dNdn(i+1,s,t,n);
+            res(3, 3*i+2) =  0;
+            res(4, 3*i+0) =  0;
+            res(4, 3*i+1) =  coef_g(s,t,n)*dNds(i+1,s,t,n) - coef_h(s,t,n)*dNdt(i+1,s,t,n) + coef_i(s,t,n)*dNdn(i+1,s,t,n);
+            res(4, 3*i+2) = -coef_d(s,t,n)-dNds(i+1,s,t,n) + coef_e(s,t,n)*dNdt(i+1,s,t,n) - coef_f(s,t,n)*dNdn(i+1,s,t,n);
+            res(5, 3*i+0) =  coef_g(s,t,n)*dNds(i+1,s,t,n) - coef_h(s,t,n)*dNdt(i+1,s,t,n) + coef_i(s,t,n)*dNdn(i+1,s,t,n);
+            res(5, 3*i+1) =  0;
+            res(5, 3*i+2) =  coef_a(s,t,n)*dNds(i+1,s,t,n) - coef_b(s,t,n)*dNdt(i+1,s,t,n) + coef_c(s,t,n)*dNdn(i+1,s,t,n);
+        }
+        res = res/Jacobi(s,t,n).determinant();
+        return res;
+    };
 }
 
 void hex8node::calDMatrix(realn E, realn nu, MatrixXr &ele_DMat) const
@@ -110,45 +135,6 @@ void hex8node::calDMatrix(realn E, realn nu, MatrixXr &ele_DMat) const
                 0, 0, 0, 0, 0, 0.5-nu;
     ele_DMat = ele_DMat*(E/((1+nu)*(1-2*nu)));
 }
-
-// #ifndef NOT_KEEP_BMat
-// void hex8node::calJacobiMat(MatrixXr &TV)
-// #else
-// void hex8node::calJacobiMat(MatrixXr &TV, hex8node::funcMat &Jacobi) const
-// #endif
-// {
-//     #define x(i) TV(node_num[i-1],0)
-//     #define y(i) TV(node_num[i-1],1)
-//     #define z(i) TV(node_num[i-1],2)
-//     std::vector<int> xi    = {0,1,1,-1,-1,1,1,-1,-1};
-//     std::vector<int> eta   = {0,-1,1,1,-1,-1,1,1,-1};
-//     std::vector<int> gamma = {0,-1,-1,-1,-1,1,1,1,1};
-//     std::vector<std::function<realn(realn,realn)>> dNds(9), dNdt(9), dNdn(9);
-//     for(int i=1; i<9; i++){
-//         dNds[i] = [&xi, &eta, &gamma, i](realn t, realn n){
-//             return xi[i]*(1+eta[i]*t)*(1+gamma[i]*n)/8;
-//         };
-//         dNdt[i] = [&xi, &eta, &gamma, i](realn s, realn n){
-//             return eta[i]*(1+xi[i]*s)*(1+gamma[i]*n)/8;
-//         };
-//         dNdn[i] = [&xi, &eta, &gamma, i](realn s, realn t){
-//             return gamma[i]*(1+xi[i]*s)*(1+eta[i]*t)/8;
-//         };
-//     }
-//     Jacobi = [&](realn s, realn t, realn n){
-//         realn dxds=0, dxdt=0, dxdn=0;
-//         realn dyds=0, dydt=0, dydn=0;
-//         realn dzds=0, dzdt=0, dzdn=0;
-//         for(int i=1; i<9; i++){
-//             dxds += x(i)*dNds[i](t,n); dxdt += x(i)*dNdt[i](s,n); dxdn += x(i)*dNdn[i](s,t);
-//             dyds += y(i)*dNds[i](t,n); dydt += y(i)*dNdt[i](s,n); dydn += y(i)*dNdn[i](s,t);
-//             dzds += z(i)*dNds[i](t,n); dzdt += z(i)*dNdt[i](s,n); dzdn += z(i)*dNdt[i](s,t);
-//         }
-//         MatrixXr result(3,3);
-//         result << dxds, dyds, dzds, dxdt, dydt, dzdt, dxdn, dydn, dzdn;
-//         return result;
-//     };
-// }
 
 } // namespace lhfea
 
